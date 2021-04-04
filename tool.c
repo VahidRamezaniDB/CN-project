3aed6abe139b434dd4f8368b9bf8801502d75a75
ConnorLynch2000 <vahid.ramezani.2014@gmail.com>
2021-04-05 00:08:44 +0430

    tool.c file created

---
+++ tool.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main(){
+	int my_socket =socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	
+
+}
