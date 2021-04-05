ecab9640b1474ace10aee0f44b6c018ccc20b390
ConnorLynch2000 <vahid.ramezani.2014@gmail.com>
2021-04-05 21:35:08 +0430

    starting configs

--- tool.c
+++ tool.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
 
 int main(){
-	int my_socket =socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 	
-
 }
