84634048775cbb72009ee150308aaece7a7254e7
ConnorLynch2000 <vahid.ramezani.2014@gmail.com>
2021-04-09 02:03:42 +0430

    getting port range from user

--- tool.c
+++ tool.c
@@ -21,7 +21,7 @@ connection
 */
 const int MAX_IP_STR_LEN=16;
 
-int hname_to_ip(char *hname,char *ip, sockaddr_in *host){
+int hname_to_ip(char *hname,char *ip,struct sockaddr_in *host){
     
     struct addrinfo hints, *servinfo, *p;
     int rv;
@@ -42,6 +42,16 @@ int hname_to_ip(char *hname,char *ip, sockaddr_in *host){
     }
 }
 
+void scan_ports(int sock_id,int port_range_start,int port_range_end,struct sockaddr_in *server_address){
+
+    int server_port=6000;   
+    server_address->sin_port=htons(server_port);
+    if(connect(sock_id,(struct sockaddr *)server_address,sizeof(server_address))<0){
+        fputs("port is close.\n",stdout);
+    }else{
+        fputs("port is active.\n",stdout);
+    }
+}
 
 int main(int argc, char *argv[]){
 
@@ -52,8 +62,9 @@ int main(int argc, char *argv[]){
 
     char *host_ip_str=malloc(sizeof(char)*MAX_IP_STR_LEN);
     char *user_in;
-    struct sockaddr_in serve_address;
+    struct sockaddr_in server_address;
     int rv;
+    int port_range_start,port_range_end;
 
     if(host_ip_str==NULL){
         fputs("memmory alloocation failed.(host_name)\n",stderr);
@@ -61,15 +72,27 @@ int main(int argc, char *argv[]){
     }
 
     memset(host_ip_str,'\0',MAX_IP_STR_LEN);
-    memset(&serve_address,0,sizeof(serve_address));
+    memset(&server_address,0,sizeof(server_address));
     user_in=argv[1];
 
-    if((rv=hname_to_ip(user_in,host_ip_str,&serve_address))!=0){
+    if((rv=hname_to_ip(user_in,host_ip_str,&server_address))!=0){
         fprintf(stderr, "host name resolving failed\n more info: %s\n", gai_strerror(rv));
         exit(EXIT_FAILURE);
     }
 
     printf("%s ip resolved as %s\n",user_in,host_ip_str);
+
+    puts("port scaning range.");
+    puts("start from:");
+    scanf("%d",&port_range_start);
+    puts("to:");
+    scanf("%d",&port_range_end);
+    if(port_range_end>65535 || port_range_end<0 || port_range_start>65535 || port_range_start<0){
+        fputs("invalid port range.",stderr);
+        exit(EXIT_FAILURE);
+    }
+    puts("OK");
+
 
     int sock=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
@@ -78,21 +101,22 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    serve_address.sin_family=AF_INET;
-    int pton_addr=inet_pton(AF_INET,host_ip_str,serve_address.sin_addr.s_addr);
+    server_address.sin_family=AF_INET;
+    int pton_addr=inet_pton(AF_INET,host_ip_str,&server_address.sin_addr.s_addr);
 
     if(pton_addr==0){
-        fputs("inavlid ip address. (pton)");
+        fputs("inavlid ip address. (pton)\n",stderr);
         exit(EXIT_FAILURE);
     }
 
     if(pton_addr<0){
-        fputs("printable to numerical failed.");
+        fputs("printable to numerical failed.\n",stderr);
         exit(EXIT_FAILURE);
     }
-
     
+    scan_ports(sock,port_range_start,port_range_end,&server_address);
 
+    close(sock);
     return 0;
 }
 
